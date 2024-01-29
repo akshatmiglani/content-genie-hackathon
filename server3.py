fe17d966125b5592ed49c7627c50d7aa6b609a3a
Akshat Miglani <120178102+akshatmiglani@users.noreply.github.com>
2024-01-29 21:19:55 +0530

    Update server3.py

--- server3.py
+++ server3.py
@@ -1,43 +1,4 @@
-# from flask import Flask, render_template
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# from flask import Flask, render_template, request
-
-# app = Flask(__name__)
-# ALLOWED_EXTENSIONS=['mp4']
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
-
-# @app.route('/upload',methods=['POST'])
-# def upload():
-#     if 'video' not in request.files:
-#         return "No video file found"
-#     video=request.files['video']
-#     if video.filename=="":
-#         return "No video selected"
-#     if video and allowed_file(video.filename):
-#         video.save('static/'+video.filename)
-#         return render_template('preview.html',video_name=video.filename)
-#     return "INVALID"
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 
 from flask import Flask, render_template, request
